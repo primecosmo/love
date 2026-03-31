522c3f9c9f53c9f74e5deaa5c94c7b02d3f4b08f
primecosmo <primecosmo40@gmail.com>
2026-03-31 22:07:19 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import Hero from "./components/Hero";
-import ApologyCard from "./components/ApologyCard";
-import LoveReasons from "./components/LoveReasons";
-import MusicPlayer from "./components/MusicPlayer";
 
 function App() {
   return (
     <div className="bg-pink-50 text-gray-800 overflow-x-hidden">
-      {/* <MusicPlayer /> */}
       <Hero />
-      {/* <ApologyCard />
-      <LoveReasons /> */}
     </div>
   );
 }
